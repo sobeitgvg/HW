58f56ebab6086b771a1432e15e0e0eadbc3e07a5
sobeitgvg <gvg.sobeit@yandex.ru>
2022-03-07 10:12:19 +0300

    les1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {Button} from './components/button/massage';
 
+function App() {
+
+  const buttonTitle = 'H-a-l-l-o';
+  const handleBtn = (text) =>{
+   alert(`${text}`)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button onButtonClick={handleBtn} title={buttonTitle}/>
+      </header>
+    </div>
   );
 } 
 
